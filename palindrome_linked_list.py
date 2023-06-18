14603e1c41f1647f693370b8a96d7f5c9b4011e4
agbleze <lagbleze@st.ug.edu.gh>
2023-06-18 23:39:36 +0200

    Update palindrome_linked_list.py

--- palindrome_linked_list.py
+++ palindrome_linked_list.py
@@ -41,6 +41,15 @@ def isPalindrome(self, head:ListNode) -> bool:
     
     while head:
         nums.append(head.val)
+        head = head.next
+        
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        if nums[l] != nums[r]:
+            return False
+        l += 1
+        r -1
+    return True
         
 
 
