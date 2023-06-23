05c94aaa763be93d58cf4a134449b5e016727cc8
agbleze <lagbleze@st.ug.edu.gh>
2023-06-23 22:58:37 +0200

    Update longest_consecutive_sequence.py

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -34,8 +34,15 @@ def longest_consecutive_sequence(nums: list) -> int:
         if (n - 1) not in hashset:
             length = 0
             while (n + length) in hashset:
-                length =+ 1
+                length += 1
             longest = max(longest, length)
+    return longest
+
+
+#%% test neetcode
+
+
+longest_consecutive_sequence(nums = [100,4,200,1,3,2])
 
 
 
@@ -50,3 +57,5 @@ def longest_consecutive_sequence(nums: list) -> int:
 
 
 
+
+# %%
