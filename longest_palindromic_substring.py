458ca4b503208cc0e6fd0a6f23098d89f9b4286d
agbleze <lagbleze@st.ug.edu.gh>
2023-06-24 07:34:12 +0200

    Create longest_palindromic_substring.py

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,75 @@
+"""
+5. Longest Palindromic Substring
+Medium
+25.7K
+1.5K
+Companies
+Given a string s, return the longest 
+palindromic
+ 
+substring
+ in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+"""
+
+#%% neetcode
+
+def longestPalindrome(s: str):
+    res = ""
+    resLen = 0
+    
+    for i in range(len(s)):
+        # odd length
+        l, r = i, i
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > resLen:
+                res = s[l:r+1]
+                resLen = r - l + 1
+            l -= 1
+            r += 1
+            
+        # even length
+        l, r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > resLen:
+                res = s[l:r+1]
+                resLen = r - l + 1
+            l -= 1
+            r += 1
+    return res
+
+
+#%% test neetcode
+
+longestPalindrome( s="cbbd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
