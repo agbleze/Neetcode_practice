63f9e646b5e778da21182f2ac0c7e48a426522e2
agbleze <lagbleze@st.ug.edu.gh>
2023-06-28 18:39:17 +0200

    Update container_with_most_water.py

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -33,7 +33,23 @@ n == height.length
 0 <= height[i] <= 104
 """
 
-#%%
+#%% neetcode
+
+def containMost(height):
+    l, r = 0, len(height) - 1
+    res = 0
+    while l < r:
+        area = (r-l) * min(height[l], height[r])
+        res = max(res, area)
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    return res
+
+#%% test
+
+containMost(height = [1,8,6,2,5,4,8,3,7])
 
 
 
@@ -49,3 +65,4 @@ n == height.length
 
 
 
+# %%
